9027b8b3ec9aa3c37426e9b1cb1163503cb2d662
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-03-29 13:39:23 +0000

    modification au niveau du fichier js concernant Fetch API

--- contact.js
+++ contact.js
@@ -1,11 +1,26 @@
-
-async function get(){
-    const response = await fetch("user.json")
-    const data = await response.json()
-    console.log(data)
-    
-    document.querySelector(".name").innerHTML = data.results[0].name.first + " " + data.results[0].name.last;
-    document.querySelector("#img").src = data.results[0].picture.large;
-    document.querySelector(".statut").innerHTML = data.results[0].name.title;
- }
- get()
+async function get() {
+    const response = await fetch("/user.json");
+    const data = await response.json();
+  
+    const cardsContainer = document.querySelector('.liste');
+  
+    // Clear the existing content inside cardsContainer
+    cardsContainer.innerHTML = '';
+  
+    // Iterate over each user in the data and create a list item for them
+    data.results.forEach(user => {
+      const flipcardElement = document.createElement("div");
+      flipcardElement.classList.add("list");
+      flipcardElement.innerHTML = `
+        <div class="image"><img src="${user.picture.medium}" alt="Profile Picture"></div>
+        <div class="info">
+          <div class="name"><h5>${user.name.first} ${user.name.last}</h5></div>
+          <div class="statut"><label>${user.location.street.name}</label></div>
+        </div>
+      `;
+      cardsContainer.appendChild(flipcardElement);
+    });
+  }
+  
+  get();
+  

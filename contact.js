04333d7371e7f502672d9b438028651d9ffaa462
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-03-31 21:25:48 +0000

    Changement du design

--- contact.js
+++ contact.js
@@ -1,30 +1,29 @@
-async function get() {
-    const response = await fetch("https://randomuser.me/api/");
-    const data = await response.json();
-  
-    const cardsContainer = document.querySelector('.liste');
-  
-    // Clear the existing content inside cardsContainer
-  
-    // Iterate over each user in the data and create a list item for them
-    data.results.forEach(user => {
-      const flipcardElement = document.createElement("div");
-      flipcardElement.classList.add("list");
-      flipcardElement.innerHTML = `
-        <div class="image"><img src="${user.picture.medium}" alt="Profile Picture"></div>
-        <div class="info">
-          <div class="name"><h5>${user.name.first} ${user.name.last}</h5></div>
-          <div class="statut"><label>${user.location.street.name}</label></div>
-        </div>
-      `;
-      cardsContainer.appendChild(flipcardElement);
-    });
-  }
-  
-  function loop() {
-    for(i=0 ; i<20 ; i++) {
-        get();
-    }
-  } 
+async function getContacts() {
+  const response = await fetch("https://randomuser.me/api/?results=6");
+  const data = await response.json();
 
-  loop()
+  const cardsContainer = document.querySelector('.row');
+
+  data.results.forEach(user => {
+    const flipcardElement = document.createElement("div");
+    flipcardElement.classList.add("col-xs-12", "col-sm-6", "col-md-4");
+    flipcardElement.innerHTML = `
+      <div class="frontside">
+        <div class="card">
+          <div class="card-body text-center">
+            <p><img class="img-fluid" src="${user.picture.medium}" alt="Profile Picture"></p>
+            <h4 class="card-title">${user.name.first} ${user.name.last}</h4>
+            <p class="card-text"><label>${user.location.street.name}</label></p>
+            <a href="https://www.fiverr.com/share/qb8D02" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+          </div>
+        </div>
+      </div>
+    `;
+    cardsContainer.appendChild(flipcardElement);
+  });
+}
+
+// Appeler la fonction au chargement de la page pour charger les contacts
+document.addEventListener('DOMContentLoaded', () => {
+  getContacts();
+});
